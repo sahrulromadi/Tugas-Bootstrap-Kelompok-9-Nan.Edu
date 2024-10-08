2964654a3497c745b1f5eab1d3046d4494a3497d
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-10-08 21:22:52 +0700

    nambah js di contact agar data yang di kirim akan muncul sebagai alert

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,8 @@
 // untuk faq
-
 // ambil semua pertanyaan dengan class faq-item
 const boxQuestions = document.querySelectorAll(".faq-item");
 
+// gunakan loop krn memiliki class yang sama
 boxQuestions.forEach((q) => {
   // ambil answer dengan query selector
   // query selector -> ambil element pertama dari faq-item
@@ -24,4 +24,16 @@ boxQuestions.forEach((q) => {
       icon.classList.add("bi-plus");
     }
   });
+});
+
+// untuk contact
+document.querySelector("form").addEventListener("submit", () => {
+  // ambil elemen berdasarkan id
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // alert yang muncul
+  alert(`Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}`);
 });
