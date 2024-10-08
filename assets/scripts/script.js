784d66c5b77d1d72b071d0f6684267902240987f
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-10-08 22:30:01 +0700

    menambah dom js untuk opacity navbar dan fix bug

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,45 +1,68 @@
-// untuk faq
-// ambil semua pertanyaan dengan class faq-item
-const boxQuestions = document.querySelectorAll(".faq-item");
+// domcontentload -> untuk memastikan seluruh element sudah ada
+document.addEventListener("DOMContentLoaded", () => {
+  // untuk FAQ
+  // ambil semua pertanyaan dengan class faq-item
+  const boxQuestions = document.querySelectorAll(".faq-item");
 
-// gunakan loop krn memiliki class yang sama
-boxQuestions.forEach((q) => {
-  // ambil answer dengan query selector
-  // query selector -> ambil element pertama dari faq-item
-  const answer = q.querySelector(".faq-answer");
-  // ambil icon
-  const icon = q.querySelector(".faq-question i");
+  // gunakan loop karena memiliki class yang sama
+  boxQuestions.forEach((q) => {
+    // ambil answer dengan query selector
+    const answer = q.querySelector(".faq-answer");
+    // ambil icon
+    const icon = q.querySelector(".faq-question i");
 
-  // event listener ketika faq-item diklik
-  q.addEventListener("click", () => {
-    if (answer.style.display === "none") {
-      answer.style.display = "block";
-      // hapus icon yang ada
-      icon.classList.remove("bi-plus");
-      // tambahkan icon baru
-      icon.classList.add("bi-dash");
+    // event listener ketika faq-item diklik
+    q.addEventListener("click", () => {
+      if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+        // hapus icon yang ada
+        icon.classList.remove("bi-plus");
+        // tambahkan icon baru
+        icon.classList.add("bi-dash");
+      } else {
+        answer.style.display = "none";
+        icon.classList.remove("bi-dash");
+        icon.classList.add("bi-plus");
+      }
+    });
+  });
+
+  // untuk contact
+  const contactForm = document.getElementById("contactForm");
+
+  // jika #contactForm ada
+  if (contactForm) {
+    contactForm.addEventListener("submit", () => {
+      // ambil elemen berdasarkan id
+      const name = document.getElementById("name").value;
+      const phone = document.getElementById("phone").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+
+      // alert yang muncul
+      alert(
+        `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}`
+      );
+
+      // di console log
+      console.log("Name:", name);
+      console.log("Phone:", phone);
+      console.log("Email:", email);
+      console.log("Message:", message);
+    });
+  }
+
+  // untuk navbar
+  // event listener scroll
+  document.addEventListener("scroll", () => {
+    // kelas navbar
+    const navbar = document.querySelector(".navbar");
+
+    // jika scroll lebih dari 5 pixel
+    if (window.scrollY > 5) {
+      navbar.style.opacity = "0.9"; // Ubah opacity
     } else {
-      answer.style.display = "none";
-      icon.classList.remove("bi-dash");
-      icon.classList.add("bi-plus");
+      navbar.style.opacity = "1"; // Kembalikan opacity
     }
   });
-});
-
-// untuk contact
-document.getElementById("contactForm").addEventListener("submit", () => {
-  // ambil elemen berdasarkan id
-  const name = document.getElementById("name").value;
-  const phone = document.getElementById("phone").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  // alert yang muncul
-  alert(`Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}`);
-
-  // di console log
-  console.log("Name:", name);
-  console.log("Phone:", phone);
-  console.log("Email:", email);
-  console.log("Message:", message);
 });
